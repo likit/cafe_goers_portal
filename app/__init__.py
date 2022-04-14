a6df4464e2f5021f225e64d320e8abc8e60265b0
Likit Preeyanon <likit.pre@mahidol.edu>
2022-04-14 17:33:48 +0700

    edited the gunicorn command
    fixed issue on wrong connection string  when deploying with Heorku postgres

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from flask_migrate import Migrate
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres:', 'postgresql:')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
