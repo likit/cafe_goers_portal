fab39232e9b57576ae4ab204ff520701fbf1c904
Likit Preeyanon <likit.pre@mahidol.edu>
2022-04-14 16:51:04 +0700

    added many more fields to add detail to the place
    updated the form accordingly

--- app/models.py
+++ app/models.py
@@ -5,6 +5,39 @@ from geoalchemy2.shape import from_shape
 from app import db
 from sqlalchemy.sql import func
 
+place_styles = db.Table('place_styles',
+    db.Column('cafe_id', db.Integer, db.ForeignKey('cafes.id'), primary_key=True),
+    db.Column('style_id', db.Integer, db.ForeignKey('styles.id'), primary_key=True)
+)
+
+place_zones = db.Table('place_zones',
+                        db.Column('cafe_id', db.Integer, db.ForeignKey('cafes.id'), primary_key=True),
+                        db.Column('zone_id', db.Integer, db.ForeignKey('zones.id'), primary_key=True)
+                        )
+
+place_activities = db.Table('place_activities',
+                       db.Column('cafe_id', db.Integer, db.ForeignKey('cafes.id'), primary_key=True),
+                       db.Column('activity_id', db.Integer, db.ForeignKey('activities.id'), primary_key=True)
+                       )
+
+
+class Style(db.Model):
+    __tablename__ = 'styles'
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    style = db.Column('style', db.String)
+
+
+class Zone(db.Model):
+    __tablename__ = 'zones'
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    zone = db.Column('zone', db.String)
+
+
+class Activity(db.Model):
+    __tablename__ = 'activities'
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
+    activity = db.Column('activity', db.String)
+
 
 class Address(db.Model):
     __tablename__ = 'addresses'
@@ -14,22 +47,6 @@ class Address(db.Model):
     district = db.Column('district', db.String, info={'label': 'Province'})
     subdistrict = db.Column('subdistrict', db.String, info={'label': 'Sub-district'})
     postal_code = db.Column('postal_code', db.String, info={'label': 'Postal Code'})
-    price_level = db.Column('price_level', db.Integer,
-                            info={'label': 'Price Level',
-                                  'choices': [(i, c) for i, c in enumerate(['$-$$', '$$-$$$', '$$$-$$$$'])],
-                                  }
-                            )
-    service_charge_rate = db.Column('service_charge_rate', db.Float(), default=0.0,
-                                    info={'label': 'Service Charge Rate'})
-    vat_included = db.Column('vat_included', db.Boolean, default=True, info={'label': 'Vat included'})
-    parking_avail = db.Column('parking_avail', db.String, info={'label': 'Parking Availability',
-                                                                'choices': [(c, c) for c in ['<10', '10-20', '>20']]})
-    parking_note = db.Column('parking_note', db.Text(), info={'label': 'Parking Note'})
-    parking_fee = db.Column('parking_fee', db.Boolean, info={'label': 'Parking Fee'})
-    free_wifi = db.Column('free_wifi', db.Boolean, info={'label': 'Free WiFi'})
-    seating_avail = db.Column('seating_avail', db.String, info={'label': 'Seating Availability',
-                                                                'choices': [(c, c) for c in ['<10', '10-20', '>20']]})
-    pet_friendly = db.Column('pet_friendly', db.Boolean, info={'label': 'Pet Friendly'})
     created_at = db.Column('created_at', db.DateTime(timezone=True),
                            server_default=func.now())
     updated_at = db.Column('updated_at', db.DateTime(timezone=True),
@@ -60,6 +77,25 @@ class Cafe(db.Model):
                            onupdate=func.now())
     address = db.relation(Address, backref=db.backref('cafe', uselist=False,
                                                       cascade='all, delete-orphan'))
+    zones = db.relation(Zone, secondary=place_zones, lazy='subquery', backref=db.backref('cafes', lazy=True))
+    styles = db.relation(Style, secondary=place_styles, lazy='subquery', backref=db.backref('cafes', lazy=True))
+    activities = db.relation(Activity, secondary=place_activities, lazy='subquery', backref=db.backref('cafes', lazy=True))
+    price_level = db.Column('price_level', db.Integer,
+                            info={'label': 'Price Level',
+                                  'choices': [(i, c) for i, c in enumerate(['$-$$', '$$-$$$', '$$$-$$$$'])],
+                                  }
+                            )
+    service_charge_rate = db.Column('service_charge_rate', db.Float(), default=0.0,
+                                    info={'label': 'Service Charge Rate'})
+    vat_included = db.Column('vat_included', db.Boolean, default=True, info={'label': 'Vat included', 'description': 'ราคารวมภาษีมูลค่าเพิ่มแล้ว'})
+    parking_avail = db.Column('parking_avail', db.String, info={'label': 'Parking Availability',
+                                                                'choices': [(c, c) for c in ['<10', '10-20', '>20']]})
+    parking_note = db.Column('parking_note', db.Text(), info={'label': 'Parking Note'})
+    parking_fee = db.Column('parking_fee', db.Boolean, info={'label': 'Parking Fee'})
+    free_wifi = db.Column('free_wifi', db.Boolean, info={'label': 'Free WiFi'})
+    seating_avail = db.Column('seating_avail', db.String, info={'label': 'Seating Availability',
+                                                                'choices': [(c, c) for c in ['<10', '10-20', '>20']]})
+    pet_friendly = db.Column('pet_friendly', db.Boolean, info={'label': 'Pet Friendly'})
 
 
 class InstagramEmbedded(db.Model):
--- app/webadmin/forms.py
+++ app/webadmin/forms.py
@@ -1,9 +1,10 @@
+from wtforms.validators import DataRequired
+
 from app import db
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms_alchemy import (model_form_factory, ModelFormField, )
+from wtforms import StringField, widgets
+from wtforms_alchemy import (model_form_factory, ModelFormField, QuerySelectMultipleField)
 from app.models import *
-
 
 BaseModelForm = model_form_factory(FlaskForm)
 
@@ -18,7 +19,6 @@ class PlaceSearchForm(FlaskForm):
     name = StringField('Place name')
 
 
-
 class AddressForm(ModelForm):
     class Meta:
         model = Address
@@ -31,6 +31,18 @@ class CafeForm(ModelForm):
         exclude = ['created_at', 'updated_at']
 
     address = ModelFormField(AddressForm)
+    zones = QuerySelectMultipleField(u'Zone', get_label='zone',
+                                     query_factory=lambda: Zone.query.all(),
+                                     widget=widgets.ListWidget(prefix_label=False),
+                                     option_widget=widgets.CheckboxInput())
+    styles = QuerySelectMultipleField(u'Style', get_label='style',
+                                      query_factory=lambda: Style.query.all(),
+                                      widget=widgets.ListWidget(prefix_label=False),
+                                      option_widget=widgets.CheckboxInput())
+    activities = QuerySelectMultipleField(u'Activities', get_label='activity',
+                                          query_factory=lambda: Activity.query.all(),
+                                          widget=widgets.ListWidget(prefix_label=False),
+                                          option_widget=widgets.CheckboxInput())
 
 
 class InstagramPostForm(ModelForm):
